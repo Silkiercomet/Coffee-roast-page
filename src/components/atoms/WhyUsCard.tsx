f4abf9184362eb567b722d487f231086153440d6
luis Colina <colina.1990lz@gmail.com>
2025-02-07 11:41:32 -0400

    Refactor WhyUsCard and WhyUsSection components; replace imageRoute with JSX.Element for dynamic rendering; add new CSS styles and animations for improved layout and responsiveness

--- src/components/atoms/WhyUsCard.tsx
+++ src/components/atoms/WhyUsCard.tsx
@@ -1,14 +1,15 @@
+import style from "./WhyUsCard.module.css"
 interface WhyUsCardProp {
-  imageRoute: string;
+  Image: JSX.Element;
   header: string;
   content: string;
 }
 
-const WhyUsCard: React.FC<WhyUsCardProp> = ({ imageRoute, header, content }) => (
-  <li>
-    <img src={imageRoute} alt={header} />
-    <h3>{header}</h3>
-    <p>{content}</p>
+const WhyUsCard: React.FC<WhyUsCardProp> = ({ Image, header, content }) => (
+  <li className={style.card}>
+    {Image}
+    <h3 className={style.card_header}>{header}</h3>
+    <p className={style.card_content}>{content}</p>
   </li>
 );
 
