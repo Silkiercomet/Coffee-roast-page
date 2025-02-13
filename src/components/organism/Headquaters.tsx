342b1fc9f6011b76c3dbcb558fcd2495cc7a8b2d
luis Colina <colina.1990lz@gmail.com>
2025-02-13 09:50:22 -0400

    Refactor Headquarters component for improved styling and layout; integrate dynamic content for HowItWorks section; update CSS for better responsiveness and visual hierarchy.

--- src/components/organism/Headquaters.tsx
+++ src/components/organism/Headquaters.tsx
@@ -1,11 +1,12 @@
 import Country from '../moleculs/Country';
 import {addressData} from "../../ultils/content"
+import style from "./Headquaters.module.css"
 const Headquarters = () => {
 
   return (
-    <section>
-      <h3>Our Headquarters</h3>
-      <ul>
+    <section className={style.headquarters}>
+      <h3 className={style.headquarters_header}>Our Headquarters</h3>
+      <ul className={style.headquarters_list}>
         {addressData.map((country, index) => (
           <Country
             key={index}
