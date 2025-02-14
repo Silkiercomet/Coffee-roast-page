c568f37ca2677640c54c12336559611ac9b963c9
luis Colina <colina.1990lz@gmail.com>
2025-02-14 16:55:28 -0400

    Refactor FormOptions, OrderSummary, and OrderForm components for improved styling and layout; integrate new CSS modules for better responsiveness and visual hierarchy.

--- src/components/moleculs/FormOptions.tsx
+++ src/components/moleculs/FormOptions.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { UseFormRegister } from 'react-hook-form';
 import {Option} from "../../ultils/content"
-
+import style from "./FormOption.module.css"
 interface FormOptionsProps {
   register: UseFormRegister<any>;
   name: string;
@@ -11,24 +11,29 @@ interface FormOptionsProps {
 
 const FormOptions: React.FC<FormOptionsProps> = ({ register, name, options, optionTitle }) => {
   const [isOpen, setIsOpen] = useState(false);
-
+  function handleOpen() {
+    setIsOpen(!isOpen)
+  }
   return (
-    <div id={name}>
-      <h3>{optionTitle}</h3>
-      <ul>
-        {options.map((option) => (
-          <li key={option.value}>
+    <div id={name} className={style.option}>
+      <h3 className={style.option_header} onClick={() => handleOpen()}>{optionTitle}</h3>
+      <div className={`${style.accordion} `}>
+        <ul className={`${style.option_list} ${isOpen ? style.active : ""}`}>
+          {options.map((option) => (
+            <li key={option.value} className={style.option_list_item}>
 
-              <input type="radio" {...register(name)} value={option.value} name={name} id={option.value}/>
-              {option.value}
+                <input type="radio" {...register(name)} value={option.value} name={name} id={option.value}/>
+                
 
-            <label htmlFor={option.value}>
-              <h4>{option.value}</h4>
-              <p>{option.content}</p>
-            </label>
-          </li>
-        ))}
-      </ul>
+              <label htmlFor={option.value} className={style.option_list_item_label}>
+                <h4>{option.value}</h4>
+                <p>{option.content}</p>
+              </label>
+            </li>
+          ))}
+        </ul>
+      </div>
+
     </div>
   );
 };
--- src/components/organism/OrderForm.tsx
+++ src/components/organism/OrderForm.tsx
@@ -1,7 +1,8 @@
 import { useForm } from 'react-hook-form';
-import { formData, FormDataSection } from '../../ultils/content';
+import { formData } from '../../ultils/content';
 import FormOptions from '../moleculs/FormOptions';
 import OrderSummary from '../moleculs/OrderSummary';
+import style from "./OrderForm.module.css"
 
 const OrderForm = () => {
   const { register, handleSubmit, watch } = useForm();
@@ -11,30 +12,32 @@ const OrderForm = () => {
   };
 
   return (
-    <section>
-      <h3>Order Form</h3>
-      <aside>
-        <ol>
+    <section className={style.order}>
+      <h3 className={style.order_header}>Order Form</h3>
+      <div className={style.wrapper}>
+        <aside className={style.order_aside}>
+          <ol>
+            {formData.map((item) => (
+              <li key={item.legend} className={style.order_aside_item}>
+                <a href={`#${item.legend}`}>{item.legend}</a>
+              </li>
+            ))}
+          </ol>
+        </aside>
+        <form onSubmit={handleSubmit(onSubmit)} className={style.form}>
           {formData.map((item) => (
-            <li key={item.legend}>
-              <a href={`#${item.legend}`}>{item.legend}</a>
-            </li>
+            <FormOptions
+              key={item.legend}
+              register={register}
+              name={item.legend}
+              options={item.options}
+              optionTitle={item.title}
+            />
           ))}
-        </ol>
-      </aside>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        {formData.map((item) => (
-          <FormOptions
-            key={item.legend}
-            register={register}
-            name={item.legend}
-            options={item.options}
-            optionTitle={item.title}
-          />
-        ))}
-        <OrderSummary watch={watch} />
-        <input type="submit" />
-      </form>
+          <OrderSummary watch={watch} />
+          <input type="submit" className={style.button} value="Create Plan"/>
+        </form>
+      </div>
     </section>
   );
 };
