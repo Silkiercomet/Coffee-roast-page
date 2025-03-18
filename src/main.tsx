4ae9d47f09eee1650ad551eb22c9f84d96d24307
luis Colina <colina.1990lz@gmail.com>
2025-03-18 19:44:16 -0400

    Add ScrollToTop component for automatic scrolling; update OrderForm and FormOption styles for improved user experience

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router';
 import './index.css'
 import Navbar from './components/organism/Navbar';
 import Footer from './components/organism/Footer';
+import ScrollToTop from './ultils/ScrollToTop';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router>
+      <ScrollToTop />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
